2a11f5eac04f93548ba731714653823e9fa5a8c1
Jozef Cipa <cipa.jozef@gmail.com>
2025-06-10 20:11:01 +0200

    feat: add bazos api

--- src/api/bazos.ts
+++ src/api/bazos.ts
@@ -1,42 +1,55 @@
-import * as cheerio from 'cheerio'
-import { Offer } from '../types'
+import * as cheerio from 'npm:cheerio'
+export interface BazosOffer {
+  title: string
+  description: string
+  img: string
+  price: string
+  location: string
+  url: string
+}
 
 async function fetchURL(url: string): Promise<cheerio.CheerioAPI> {
-    const response = await fetch(url)
-    const html = await response.text()
-    return cheerio.load(html)
-  }
-  
-  function parseOffers($: cheerio.CheerioAPI): Offer[] {
-    const offers = $('.inzeraty')
-  
-    const results: Offer[] = []
-  
-    offers.each((_, element) => {
-      const title = $(element).find('.nadpis').text()
-      const descr = $(element).find('.popis').text()
-      const img = $(element).find('.obrazek').attr('src')
-      const price = $(element).find('.inzeratycena').text().trim()
-      const location = $(element).find('.inzeratylok').html().replace(
-        /<br>/g,
-        ', ',
-      )
-      const url = $(element).find('.inzeratynadpis a').attr('href')
-  
-      results.push({
-        title,
-        descr,
-        img,
-        price,
-        location,
-        url,
-      })
+  const response = await fetch(url)
+  const html = await response.text()
+  return cheerio.load(html)
+}
+
+function parseOffers($: cheerio.CheerioAPI): BazosOffer[] {
+  const offers = $('.inzeraty')
+
+  const results: BazosOffer[] = []
+
+  offers.each((_, element) => {
+    const title = $(element).find('.nadpis').text()
+    const description = $(element).find('.popis').text()
+    const img = $(element).find('.obrazek').attr('src') ?? ''
+    const price = $(element).find('.inzeratycena').text().trim()
+    const location = $(element).find('.inzeratylok').html()?.replace(
+      /<br>/g,
+      ', ',
+    ) ?? ''
+    const url = $(element).find('.inzeratynadpis a').attr('href')!
+
+    results.push({
+      title,
+      description,
+      img,
+      price,
+      location,
+      url,
     })
-  
-    return results
-  }
+  })
+
+  return results
+}
+
+export const isValidBazosURL = (url: string): boolean => {
+  // url needs to be either bazos.cz/search.php or bazos.sk/search.php, the rest of the URL is not important
+  const regex = /^(https?:\/\/)?(www\.)?(bazos\.cz|bazos\.sk)\/search\.php/
+  return regex.test(url)
+}
 
 export const fetchOffers = async (url: string) => {
-    const $ = await fetchURL(url)
-    return parseOffers($)
+  const $ = await fetchURL(url)
+  return parseOffers($)
 }
